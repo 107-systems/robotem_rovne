3743f0e5c35d0fc6be0cd1c1129cdd449ee65cc0
Alexander Entinger <consulting@lxrobotics.com>
2024-05-13 08:40:19 +0200

    Ensure we update with --rebase and that we are in the right directory.

--- docker/run.sh
+++ docker/run.sh
@@ -6,12 +6,13 @@ if [ "$1" = "build" ]
 then
   echo "Rebuilding package robotem_rovne ..."
   cd src/robotem_rovne
-  git pull origin main
+  git pull origin main --rebase
   cd /tmp/colcon_ws
   colcon build --packages-select robotem_rovne
 elif [ "$1" = "run" ]
 then
   echo "Starting application robotem_rovne"
+  cd /tmp/colcon_ws
   . install/setup.sh
   ros2 launch robotem_rovne all.py
 else
