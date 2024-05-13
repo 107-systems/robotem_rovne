24786aa49d629b564ac7d7a96bab5f7dcc4cf8fd
Alexander Entinger <consulting@lxrobotics.com>
2024-05-13 08:34:53 +0200

    Adapt syntax to "sh".

--- docker/run.sh
+++ docker/run.sh
@@ -2,14 +2,14 @@
 cd /tmp/colcon_ws
 . /opt/ros/humble/setup.sh
 
-if [[ "$1" = "build" ]]
+if [ "$1" = "build" ]
 then
   echo "Rebuilding package robotem_rovne ..."
   cd src/robotem_rovne
   git pull origin main
   cd /tmp/colcon_ws
   colcon build --packages-select robotem_rovne
-elif [[ "$1" = "run" ]]
+elif [ "$1" = "run" ]
 then
   echo "Starting application robotem_rovne"
   . install/setup.sh
