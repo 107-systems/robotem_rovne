1c4d79158b47b4a51205a2b1d0074c60f8d978a9
Alexander Entinger <consulting@lxrobotics.com>
2024-06-19 19:06:21 +0200

    Fix: use 192.168.1.5 as static IP address.

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -50,7 +50,7 @@ modprobe spidev
 chmod ugo+rw /dev/spidev0.0
 
 ifconfig eth0 down
-ifconfig eth0 192.168.1.1 netmask 255.255.255.0
+ifconfig eth0 192.168.1.5 netmask 255.255.255.0
 ifconfig eth0 up
 sudo -u fio ifconfig eth0
 
